59cc5ef7ed8879bfc804c05b941f7f3a3794a52d
Derek Araujo <derekaraujo19@gmail.com>
2022-09-16 14:05:16 -0400

    lab complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_valid_method
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +19,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_valid_method(invalid)
+    render json:{errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
 end
